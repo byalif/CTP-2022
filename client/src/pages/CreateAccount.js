06e8b21f50c94bf878f2f9e8a68c16ea0c25ace0
byalif <alif.rahi68@qmail.cuny.edu>
2022-12-10 16:50:14 -0500

    added create

--- client/src/pages/CreateAccount.js
+++ client/src/pages/CreateAccount.js
@@ -1,10 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import Img from "../Img";
 import Layout from "../components/layout";
 
 const CreateAccount = () => {
-  const createAcc = () => {
-    alert("hi");
+  const [input, setInput] = useState({
+    email: "",
+    lastName: "",
+    firstName: "",
+    nickName: "",
+    phone: "",
+    password: "",
+  });
+
+  const createAcc = async () => {
+    if (
+      input.email.trim() === "" ||
+      input.password.trim() === "" ||
+      input.lastName.trim() === "" ||
+      input.firstName.trim() === "" ||
+      input.phone.trim() === "" ||
+      input.nickName.trim() === ""
+    ) {
+      alert("No blank fields allowed!");
+      return;
+    }
+
+    try {
+      const login = await axios.post(
+        "https://ctp-project.herokuapp.com/api/create",
+        {
+          email: input.email,
+          password: input.password,
+          firstName: input.firstName,
+          lastName: input.lastName,
+          phone: input.phone,
+          nickName: input.nickName,
+        }
+      );
+      alert("user created!");
+    } catch (error) {
+      alert("oops");
+    }
+  };
+  const onChangeHandler = (e) => {
+    setInput({
+      ...input,
+      [e.target.name]: e.target.value,
+    });
   };
   return (
     <Layout>
@@ -12,19 +54,19 @@ const CreateAccount = () => {
         <div id="register">
           <h3>Create Account</h3>
           <form method="post" name="userRegistrationForm">
-            <label>Name</label>
-            <input type="text" name="username" />
+            <label>First Name</label>
+            <input type="text" value={input.firstName} name="firstName" />
             <label>Last name</label>
-            <input type="text" name="lastname" />
+            <input type="text" value={input.lastName} name="lastname" />
             <label>Email:</label>
-            <input type="text" name="emailid" />
+            <input type="text" value={input.email} name="email" />
             <div className="errorMsg"></div>
             <label>Nickname:</label>
-            <input type="text" name="nickname" />
+            <input type="text" value={input.nickName} name="nickName" />
             <label>Mobile No:</label>
-            <input type="text" name="mobileno" />
+            <input type="text" value={input.phone} name="phone" />
             <label>Password</label>
-            <input type="password" name="password" />
+            <input type="password" value={input.password} name="password" />
             <div className="errorMsg"></div>
             <input
               onClick={createAcc}

f6a023df8f28307c6161376409f503506057a9fa
byalif <alif.rahi68@qmail.cuny.edu>
2022-12-10 15:43:25 -0500

    idk

---
+++ client/src/data.js
@@ -0,0 +1,11 @@
+export default [
+    {
+        id: 11111111,
+        name: "Kristina Korzhenevskaya",
+        description: "Hi all, I hope you'll enjoy my outfit <3",
+        age: 31,
+        avatar: "https://images.unsplash.com/photo-1518020382113-a7e8fc38eac9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=834&q=80"
+
+    }
+
+]

08698adc95f8b9db9c3e8bfe364ed9d0e2c8e41e
byalif <alif.rahi68@qmail.cuny.edu>
2022-12-14 02:27:12 -0500

    added register

--- client/src/pages/CreateAccount.js
+++ client/src/pages/CreateAccount.js
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import Img from "../Img";
 import Layout from "../components/layout";
 import axios from "axios";
 
 const CreateAccount = () => {
+  const nav = useNavigate();
   const [input, setInput] = useState({
     email: "",
     lastName: "",
@@ -13,7 +15,8 @@ const CreateAccount = () => {
     password: "",
   });
 
-  const createAcc = async () => {
+  const createAcc = async (e) => {
+    e.preventDefault();
     if (
       input.email.trim() === "" ||
       input.password.trim() === "" ||
@@ -28,7 +31,7 @@ const CreateAccount = () => {
 
     try {
       const login = await axios.post(
-        "https://ctp-project.herokuapp.com/api/create",
+        "https://ctp-project.herokuapp.com/api/users/create",
         {
           email: input.email,
           password: input.password,
@@ -38,12 +41,15 @@ const CreateAccount = () => {
           nickName: input.nickName,
         }
       );
-      alert("user created!");
+      localStorage.setItem("id", login.data.userId);
+      localStorage.setItem("nickname", login.data.nickName);
+      nav("/");
     } catch (error) {
-      alert("oops");
+      console.log(error);
     }
   };
   const onChangeHandler = (e) => {
+    e.preventDefault();
     setInput({
       ...input,
       [e.target.name]: e.target.value,
@@ -53,71 +59,76 @@ const CreateAccount = () => {
     <Layout>
       <div id="main-registration-container">
         <div id="register" className="d-flex justify-center">
-          <form method="post" name="userRegistrationForm" > 
-          <div className="d-flex flex-wrap">
-            <div className="d-flex flex-column field-box p-2">
+          <form method="post" name="userRegistrationForm">
+            <div className="d-flex flex-wrap">
+              <div className="d-flex flex-column field-box p-2">
                 <label>First Name</label>
                 <input
-                placeholder="First Name" 
-                type="text"
-                onChange={onChangeHandler}
-                value={input.firstName}
-                name="firstName"
+                  placeholder="First Name"
+                  type="text"
+                  onChange={onChangeHandler}
+                  value={input.firstName}
+                  name="firstName"
                 />
-            </div>
-            <div className="d-flex flex-column field-box p-2">
+              </div>
+              <div className="d-flex flex-column field-box p-2">
                 <label>Last name</label>
-                    <input
-                    placeholder="Last name" 
-                    type="text"
-                    onChange={onChangeHandler}
-                    value={input.lastName}
-                    name="lastName"
-                    />
-            </div>
-            <div className="d-flex flex-column field-box p-2">
+                <input
+                  placeholder="Last name"
+                  type="text"
+                  onChange={onChangeHandler}
+                  value={input.lastName}
+                  name="lastName"
+                />
+              </div>
+              <div className="d-flex flex-column field-box p-2">
                 <label>Email:</label>
                 <input
-                placeholder="Email" 
-                type="text"
-                onChange={onChangeHandler}
-                value={input.email}
-                name="email"
+                  placeholder="Email"
+                  type="text"
+                  onChange={onChangeHandler}
+                  value={input.email}
+                  name="email"
                 />
-            </div>
-            <div className="d-flex flex-column field-box p-2">
+              </div>
+              <div className="d-flex flex-column field-box p-2">
                 <label>Nickname:</label>
                 <input
-                placeholder="Nickname" 
-                type="text"
-                onChange={onChangeHandler}
-                value={input.nickName}
-                name="nickName"
+                  placeholder="Nickname"
+                  type="text"
+                  onChange={onChangeHandler}
+                  value={input.nickName}
+                  name="nickName"
                 />
-            </div>
-            <div className="d-flex flex-column field-box p-2">
+              </div>
+              <div className="d-flex flex-column field-box p-2">
                 <label>Mobile Phone:</label>
                 <input
-                placeholder="Mobile phone" 
-                type="text"
-                onChange={onChangeHandler}
-                value={input.phone}
-                name="phone"
+                  placeholder="Mobile phone"
+                  type="text"
+                  onChange={onChangeHandler}
+                  value={input.phone}
+                  name="phone"
                 />
-            </div>
-            <div className="d-flex flex-column field-box p-2">
+              </div>
+              <div className="d-flex flex-column field-box p-2">
                 <label>Password</label>
-                <input type="password" value={input.password}  placeholder="Password" name="password" />
-                <div className="errorMsg"></div>
-            </div>
-            </div>
                 <input
-                onClick={createAcc}
-                type="submit"
-                className="button m-3"
-                value="Register"
+                  type="password"
+                  onChange={onChangeHandler}
+                  value={input.password}
+                  placeholder="Password"
+                  name="password"
                 />
-
+                <div className="errorMsg"></div>
+              </div>
+            </div>
+            <input
+              onClick={createAcc}
+              type="submit"
+              className="button m-3"
+              value="Register"
+            />
           </form>
         </div>
       </div>
